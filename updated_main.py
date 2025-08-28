63d32bb9f70ba4f532fd91026d11cf471db5401f
Kunal Bhardwaj <kunalbhardwaj2610@gmail.com>
2025-08-28 19:01:06 +0530

    Update updated_main.py

--- updated_main.py
+++ updated_main.py
@@ -694,7 +694,7 @@ st.markdown("""
         background-clip: text;
         -webkit-text-fill-color: transparent;
         color: transparent;
-    ">🤖
+    ">🤖<span>
     <h1 class='elegant-heading' style="margin: 0rem;">HCIL IT Helpdesk AI Assistant</h1>
 </div>
 """, unsafe_allow_html=True)
