b14da6cede99cc0fc89576e86b3726ff4162ab22
Kunal Bhardwaj <kunalbhardwaj2610@gmail.com>
2025-08-29 11:50:49 +0530

    Update updated_main.py

--- updated_main.py
+++ updated_main.py
@@ -719,7 +719,7 @@ for key, val in defaults.items():
         st.session_state[key] = val
 
 # Main Title with Animation
-st.markdown(""" <div style="display: flex; justify-content: right; align-items: right;"> <span class="loading-wave" style=" font-size: 10rem; background: linear-gradient(135deg, #b71c1c 0%, #e53935 50%, #f87171 100%); -webkit-background-clip: text; background-clip: text; -webkit-text-fill-color: transparent; color: transparent; ">🤖</span> <h1 class='elegant-heading'>HCIL IT Helpdesk AI Assistant</h1> </div> """, unsafe_allow_html=True)
+st.markdown(""" <div style="display: flex; justify-content: right; align-items: right; gap: -3rem;"> <span class="loading-wave" style=" font-size: 11rem; background: linear-gradient(135deg, #b71c1c 0%, #e53935 50%, #f87171 100%); -webkit-background-clip: text; background-clip: text; -webkit-text-fill-color: transparent; color: transparent; ">🤖</span> <h1 class='elegant-heading'>HCIL IT Helpdesk AI Assistant</h1> </div> """, unsafe_allow_html=True)
 
 
 st.markdown('<div class="transparent-spacer1"></div>', unsafe_allow_html=True)
